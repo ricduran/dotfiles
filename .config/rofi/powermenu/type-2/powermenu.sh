fbce88f0aab8d62b41aebee36dd814570c00e5e6
ricduran <rduranc@gmail.com>
2023-07-11 18:52:36 -0500

    os config v2

--- .config/rofi/powermenu/type-2/powermenu.sh
+++ .config/rofi/powermenu/type-2/powermenu.sh
@@ -19,13 +19,13 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
 # Options
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
+shutdown='⏻'
+reboot=''
+lock=''
+suspend='󱖒'
+logout='󰗽'
 yes=''
-no=''
+no=''
 
 # Rofi CMD
 rofi_cmd() {
